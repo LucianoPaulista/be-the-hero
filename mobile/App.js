0ecb8e96d893b147002924356b66830fbad2b922
unknown <lucianodiaspaulista@gmail.com>
2020-03-27 00:40:18 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import 'intl';
+import 'intl/locate-dat/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+  <Routes />
+  );
+}
